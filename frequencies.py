36060c389797a89d43526c1dd79e6b660e92290c
asmeret.gebremariam@kcl.ac.uk <asmeret.gebremariam@kcl.ac.uk>
2022-10-10 22:20:34 +0100

    Solution to frequency counter problem

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,17 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items):
-    frequencies = {}
-    # Your code goes here
-    return frequencies
+def frequencies(items):    
+	items = [str(item) for item in items]
+	keys = set(list(items))
+
+	frequencies = { key : 0 for key in keys}
+
+	for item in items:
+		frequencies[item]+=1
+
+	return frequencies
+
+if __name__ == '__main__':
+	res = frequencies(['0', 4,4,'4','d','d','e',0,'a','d','4'])
+	print(res)
